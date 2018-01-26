0b13819bda65bb1556c6b9233596e32fa785e6c4
OksanaZakh <zaharchenkoop@gmail.com>
2018-01-26 17:15:25 +0200

    Objects identification

--- app/src/main/java/com/example/administrator/readaloud/ReadSectionFragment.java
+++ app/src/main/java/com/example/administrator/readaloud/ReadSectionFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 23.01.2018.
@@ -13,11 +16,24 @@ import android.view.ViewGroup;
 
 public class ReadSectionFragment extends Fragment {
 
+    TextView readTextView;
+    SeekBar timeSeekBar;
+    ImageButton startReadButton;
+    ImageButton restartReadButton;
+    ImageButton pauseReadButton;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View viewRoot = inflater.inflate(R.layout.fragment_read_section_main, container, false);
+
+        readTextView = viewRoot.findViewById(R.id.ReadSectionFagment_ReadingTextView);
+        timeSeekBar = viewRoot.findViewById(R.id.ReadSectionFragment_TimeSeekBar);
+        startReadButton = viewRoot.findViewById(R.id.ReadSectionFragment_ButtonStart);
+        restartReadButton = viewRoot.findViewById(R.id.ReadSectionFragment_ButtonRestart);
+        pauseReadButton = viewRoot.findViewById(R.id.ReadSectionFragment_ButtonPause);
 
         return viewRoot;
     }
--- app/src/main/java/com/example/administrator/readaloud/WelcomeUserAvatarDialogFragment.java
+++ app/src/main/java/com/example/administrator/readaloud/WelcomeUserAvatarDialogFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
 
 
 /**
@@ -14,11 +16,25 @@ import android.view.ViewGroup;
 
 public class WelcomeUserAvatarDialogFragment extends DialogFragment {
 
+    Button cancelButton;
+    GridView avatarsGridView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.dialog_fragment_welcome_user_ava_main, container, false);
-        return view;
+        View viewRoot = inflater.inflate(R.layout.dialog_fragment_welcome_user_ava_main, container, false);
+
+        avatarsGridView = viewRoot.findViewById(R.id.WelcomeUserAvatarDialogFragment_avaGridView);
+        cancelButton = viewRoot.findViewById(R.id.WelcomeUserAvatarDialogFragment_cancelButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        return viewRoot;
     }
 }
